380501167d558d4149978bdaf43f13da3318016a
dladislav <121800755+dladislav@users.noreply.github.com>
2023-07-09 17:36:52 +0200

    first commit

---
+++ neschool web/neschool web/source.js
@@ -0,0 +1,20 @@
+
+//slider start
+var swiper = new Swiper('.blog-slider', {
+  spaceBetween: 30,
+  effect: 'fade',
+  loop: true,
+  mousewheel: {
+    invert: false,
+  },
+  // autoHeight: true,
+  pagination: {
+    el: '.blog-slider__pagination',
+    clickable: true,
+  }
+});
+//slider end
+
+
+
+
